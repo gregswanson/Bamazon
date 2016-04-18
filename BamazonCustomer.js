e28a28945788c2ceca78f88da671251eb9b8cb79
Gregory Swanson <gswanson@gregorys-mbp.fios-router.home>
2016-04-18 12:06:43 -0400

    updated table

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -22,18 +22,26 @@ prompt.start();
    				"Price: $" + rows[i].Price);
 }
 
-  // 
-  // Get two properties from the user: username and email 
-  // 
+  // Get two properties from the user: ID and Quantity 
+ 
   prompt.get(['please_enter_id_of_product', 'how_many_would_you_like'], function (err, result) {
     var product = result.please_enter_id_of_product;
-    var quantity = result.how_many_would_you_like;
+    var quantity = parseInt(result.how_many_would_you_like);
     
 		connection.query("SELECT * FROM products WHERE ItemID='" + product + "'", function(errTwo, rowsTwo, fieldsTwo) {
 		  if (err) throw err;
 		 	if (quantity <= rowsTwo[0].StockQuantity){
-		 			  console.log("You Selected: " + quantity + " " + rowsTwo[0].ProductName +
+			 		
+//change quantity
+			 		connection.query("UPDATE products SET StockQuantity=StockQuantity-2 WHERE ItemID='" + product + "'", function(err, rows, fields) {
+					  if (err) throw err;
+//log order	 
+					  console.log("You Selected: " + quantity + " " + rowsTwo[0].ProductName +
 		 			  	" Total: $" + quantity*rowsTwo[0].Price);
+					});
+
+
+		 			  
 		 			} else {
 		 				console.log("Insufficient quantity. The order has been cancelled.");
 		 			}
@@ -51,16 +59,3 @@ prompt.start();
 //connection.end();
 
 
-
-
-
-
-
-
-// connection.query('SELECT * FROM products', function(err, rows, fields) {
-//   if (err) throw err;
- 
-//   console.log(rows);
-// });
- 
-// connection.end();
